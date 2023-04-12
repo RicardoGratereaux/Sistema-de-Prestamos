353202acea90a541ebf6983bb1170f3e8c25b6e8
jearinAlvarado <jearinalvarado110403@gmail.com>
2023-04-12 17:47:10 -0500

    Segundo commit

---
+++ Sistema de Prestamos/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Sistema_de_Prestamos.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Sistema_de_Prestamos.DAL.GeneralContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Sistema_de_Prestamos.DAL.GeneralContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
