11772edf18ae69725446d94d711d37fb6165de6d
Ricardo Gratereaux Baez <20210326@itla.edu.do>
2023-04-21 18:16:39 -0600

    Cambios de lógica y vistas

--- Sistema de Prestamos/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Sistema_de_Prestamos.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
